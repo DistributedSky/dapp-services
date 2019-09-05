765dd29d8575e53ce526ed6babbf1ae14cff65a5
vol4tim <vol4tim@gmail.com>
2019-09-05 13:21:02 +0400

    update eslint
    Signed-off-by: vol4tim <vol4tim@gmail.com>

--- src/utils/rosBag.js
+++ src/utils/rosBag.js
@@ -1,5 +1,5 @@
 import { open } from 'rosbag';
-import rosbagWrite from 'rosbag-write';
+import Bag, { Time, messages } from 'rosbag-write';
 
 export default (data, cb, options = {}) => {
   return open(data).then(bag => {
@@ -10,10 +10,10 @@ export default (data, cb, options = {}) => {
 };
 export const getRosbag = async data => {
   try {
-    const bag = new rosbagWrite.Bag();
-    await rosbagWrite.messages.init();
-    const Message = await rosbagWrite.messages.getMessage('std_msgs/String');
-    const time = rosbagWrite.Time.fromSecs(1.1);
+    const bag = new Bag();
+    await messages.init();
+    const Message = await messages.getMessage('std_msgs/String');
+    const time = Time.fromSecs(1.1);
     Object.keys(data).forEach(topic => {
       bag.write('/' + topic, new Message({ data: data[topic] }), time);
       time.nsec += 100000000;
@@ -21,6 +21,6 @@ export const getRosbag = async data => {
     bag.close();
     return bag.file.getBuffer();
   } catch (error) {
-    throw error;
+    return new Error(error);
   }
 };
